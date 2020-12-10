546b8ac8a1e792d0298323eb5ddb8a5b15f42339
Panda58dev <kirill.l.06@mail.ru>
2020-12-10 13:13:00 +0300

    test block-sheme

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php 
 
 //тест массивов
-$array[0] = 'a';
+$array[0] = NULL;
 $array[1] = 'b'; 
 echo $array[0]."</br>";
 
@@ -14,9 +14,19 @@ echo "</br>";
 if ($array[0] == 'a') {
     echo  "{ $array[0] } это ячейка массива";
 } elseif ($array[0] == NULL) {
-    echo "{ $array[0] } это ячейка массива с значение NULL";
+    echo "{ $array[0] } это ячейка массива с значение NULL</br>";
 } else {
-    echo "В $array нету ячейки [0]";
+    echo "В $array нету ячейки [0] </br>";
 }
 
+
+//тест блок-схемы
+$S = 0;
+$i = 1;
+
+while ($i <= 5) {
+	$S = $i*$i+$S;
+	$i ++;
+}
+echo $S;
 ?>
